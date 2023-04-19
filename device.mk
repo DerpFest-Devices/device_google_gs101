64d2883a582bb7e114e6122d29b1efc6a6a981b9
Matheus <mattvieira123@gmail.com>
2023-04-19 01:32:27 +0000

    gs101: Avoid building Flash package
    * Gcam is much better

--- device.mk
+++ device.mk
@@ -852,6 +852,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	Iwlan
 
+# Remove some prebuilt apps
+PRODUCT_PACKAGES += \
+	 RemovePackagesDevice
+
+
 #Iwlan test app for userdebug/eng builds
 ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
